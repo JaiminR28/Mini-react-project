de50cfe5879c0019748fcb76e4e9a981f6818b75
JaiminR28 <jaiminrathwa28@gmail.com>
2023-09-15 23:47:14 +0530

    04- Menu project complete

--- 04-menu/setup/src/Menu.js
+++ 04-menu/setup/src/Menu.js
@@ -1,11 +1,10 @@
 import React from "react";
 
 const Menu = ({ items }) => {
-	console.log(items);
 	return (
 		<div className="section-center">
 			{items.map((menuItem) => {
-				const { id, title, category, price, img, desc } = menuItem;
+				const { id, title, price, img, desc } = menuItem;
 				return (
 					<article key={id} className="menu-item">
 						<img src={img} alt={title} className="photo" />
