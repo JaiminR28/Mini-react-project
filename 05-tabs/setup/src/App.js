1ecdfb3d02fa55ab51d10c6a3ed2bb96fd4920a6
JaiminR28 <jaiminrathwa28@gmail.com>
2023-09-16 21:52:05 +0530

    05- data fetched

--- 05-tabs/setup/src/App.js
+++ 05-tabs/setup/src/App.js
@@ -1,10 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+const url = "https://course-api.com/react-tabs-project";
+
 function App() {
-  return <h2>tabs project setup</h2>
+	const [loading, setLoading] = useState(true);
+	const [jobs, setJobs] = useState([]);
+	const [value, setValue] = useState(0);
+
+	const fetchJobs = async () => {
+		const response = await fetch(
+			"https://course-api.com/react-tabs-project"
+		);
+		const data = await response.json();
+		setJobs(data);
+		setLoading(false);
+	};
+
+	useEffect(() => {
+		fetchJobs();
+	}, []);
+
+	if (loading) {
+		return (
+			<section>
+				<h1>loading...</h1>
+			</section>
+		);
+	}
+
+	return <h1>Jobs</h1>;
 }
 
-export default App
+export default App;
