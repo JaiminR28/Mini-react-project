e70c3a0497fef193597ee681b93fd1787c8e0b9d
JaiminR28 <jaiminrathwa28@gmail.com>
2023-09-18 23:58:09 +0530

    05-tabs jobs  desplayed

--- 05-tabs/setup/src/App.js
+++ 05-tabs/setup/src/App.js
@@ -30,7 +30,33 @@ function App() {
 		);
 	}
 
-	return <h1>Jobs</h1>;
+	const { company, dates, duties, title } = jobs[value];
+
+	return (
+		<section className="section">
+			<div className="title">
+				<h2>expirence</h2>
+				<div className="underline"></div>
+			</div>
+			<div className="jobs-center">
+				{/** btn container */}
+				{/** job info */}
+				<article className="job-info">
+					<h3>{title}</h3>
+					<h4>{company}</h4>
+					<p className="job-date">{dates}</p>
+					{duties.map((duty, index) => {
+						return (
+							<div className="job-desc" key={index}>
+								<FaAngleDoubleRight className="job-icon" />
+								<p>{duty}</p>
+							</div>
+						);
+					})}
+				</article>
+			</div>
+		</section>
+	);
 }
 
 export default App;
