f1a2213f2c0771b0fe65bb45c69eeaeada174fe7
JaiminR28 <jaiminrathwa28@gmail.com>
2023-10-04 23:48:21 +0530

    10- replacing some of the hard coded links

--- 10-navbar/setup/src/Navbar.js
+++ 10-navbar/setup/src/Navbar.js
@@ -13,6 +13,19 @@ const Navbar = () => {
 						<FaBars />
 					</button>
 				</div>
+				<div className="links-container show-container">
+					<ul className="links">
+						{links.map((link) => {
+							const { id, url, text } = link;
+
+							return (
+								<li key={id}>
+									<a href={url}>{text}</a>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
 
 				<ul className="social-icons">
 					<li>
