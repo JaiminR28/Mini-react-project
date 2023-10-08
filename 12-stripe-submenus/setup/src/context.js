7a059429b932a1fe66b8d983961786ed89e0b2f0
JaiminR28 <jaiminrathwa28@gmail.com>
2023-10-08 23:15:12 +0530

    12 - structure and context created

--- 12-stripe-submenus/setup/src/context.js
+++ 12-stripe-submenus/setup/src/context.js
@@ -1,2 +1,25 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ childeren }) => {
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const setSidebarOpen = () => {
+		setIsSidebarOpen(true);
+	};
+
+	const setSidebarClose = () => {
+		setIsSidebarOpen(false);
+	};
+	const setModalOpen = () => {
+		setIsModalOpen(true);
+	};
+
+	const setModalClose = () => {
+		setIsModalOpen(false);
+	};
+	return <AppContext.Provider>{childeren}</AppContext.Provider>;
+};
