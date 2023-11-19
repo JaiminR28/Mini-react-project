277e8cb33b27af27915235c4b30aedf602a4b43f
JaiminR28 <jaiminrathwa28@gmail.com>
2023-11-19 22:50:45 +0530

    13 fetch data using sync/await

--- 13-cart/setup/src/reducer.js
+++ 13-cart/setup/src/reducer.js
@@ -61,6 +61,14 @@ const reducer = (state, action) => {
 			return { ...state, total, amount };
 		}
 
+		case "LOADING": {
+			return { ...state, loading: true };
+		}
+
+		case "DISPLAY_ITEMS": {
+			return { ...state, cart: action.payload, loading: false };
+		}
+
 		default:
 			return state;
 	}
